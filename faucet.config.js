5ef22495ec100187776d67eb373d73867aed896f
FND <fnd@localhost.localdomain>
2018-06-10 10:50:16 +0200

    switched to Sass

--- faucet.config.js
+++ faucet.config.js
@@ -1,6 +1,10 @@
 "use strict";
 
 module.exports = {
+	sass: [{
+		source: "./styles/index.scss",
+		target: "./dist/quaynaut.css"
+	}],
 	js: [{
 		source: "./src/index.js",
 		target: "./dist/quaynaut.js"
